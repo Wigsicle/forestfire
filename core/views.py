eccb9e4dd491be574a2fd42a0b28cb223fac65e7
Richard <wigsicle@gmail.com>
2026-07-13 16:22:58 +0800

    Argo CD test

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 
 def health(request):
     return JsonResponse({
-        "status": "ok",
+        "status": "2ok",
         "version": os.getenv("APP_VERSION", "unknown"),
         "pod": os.getenv("POD_NAME", "unknown"),
         "namespace": os.getenv("POD_NAMESPACE", "unknown"),
